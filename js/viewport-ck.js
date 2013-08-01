9053b5c1a617ef9ed01eaa3dd754e530cc99f604
NickisNoble <nick@lanyapcreative.com>
2013-08-01 00:52:31 -0500

    - put together basic structure
    - implement panelSnap
    - add type kit, fonts
    - write veiwport js

---
+++ js/viewport-ck.js
@@ -0,0 +1,1 @@
+var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName("body")[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;$("section").css("height",y).css("width",x);
